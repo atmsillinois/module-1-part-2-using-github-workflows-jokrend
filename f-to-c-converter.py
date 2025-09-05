5b3f8b9704f4834b50e0ed0270e54b1443f3d4c6
Josh Okrend <joshokrend@gmail.com>
2025-09-04 22:02:41 -0400

    added fahrenheit to celcius calculator python code

---
+++ f-to-c-converter.py
@@ -0,0 +1,20 @@
+def main():
+    print('Please enter a number in degrees Fahrenheit')
+    user_input = input()
+
+    try:
+        val = float(user_input)
+        result = f_to_c(val)
+        print(f'{user_input} degrees F is equal to {result} degrees C')
+    except ValueError:
+        print('Please enter a valid number.')
+
+
+def f_to_c(x):
+    """Converts a number from degrees F to degrees C"""
+    y = (x - 32) * 5 / 9
+    return y
+
+
+if __name__ == '__main__':
+    main()
